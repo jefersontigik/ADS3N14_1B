cd1547ec18846f0274c9e720339d0d84fd5f2304
Jeferson Tigik <jefersontigik@gmail.com>
2014-03-13 19:23:56 -0300

    Criacao da pasta do Trabalho1

---
+++ Alg3_N_B_T1/src/com/senac/estrutura/Controle/GravaArquivo.java
@@ -0,0 +1,49 @@
+package com.senac.estrutura.Controle;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.senac.estrutura.Modelo.ListaTel;
+
+public class GravaArquivo {
+
+	FileWriter arquivoW;
+	PrintWriter gravaArquivo;
+	FileReader arquivoR;
+	BufferedReader lerArquivo;
+
+	public void abreArquivoW(String caminho) throws IOException {
+		arquivoW = new FileWriter(caminho, true);
+		gravaArquivo = new PrintWriter(arquivoW);
+	}
+
+	public void abreArquivoR(String caminho) throws IOException {
+		arquivoR = new FileReader(caminho);
+		lerArquivo = new BufferedReader(arquivoR);
+	}
+
+	public void gravaArquivo(ListaTel agenda) {
+
+		gravaArquivo.println(agenda.getNome() + ";" + agenda.getFone());
+	}
+
+	public String lerArquivo() throws IOException {
+		String linha = lerArquivo.readLine();
+		if (linha == null) {
+			throw new IOException("Fim Arquivo");
+		} else {
+			return linha;
+		}
+	}
+
+	public void fechaArquivoW() {
+		gravaArquivo.close();
+	}
+
+	public void fechaArquivoR() throws IOException {
+		lerArquivo.close();
+	}
+}
