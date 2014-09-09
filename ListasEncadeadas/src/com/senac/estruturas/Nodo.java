b60b004a4ff95c61effbaa9e4b8dc9c692794203
Jeferson Tigik <jefersontigik@gmail.com>
2014-09-09 19:16:32 -0300

    Revert "Trabalho Listas Duplamente Encadeadas"
    This reverts commit e8eb7147e41866adc76c6912a087d100bce6eca6.

--- ListasEncadeadas/src/com/senac/estruturas/Nodo.java
+++ ListasEncadeadas/src/com/senac/estruturas/Nodo.java
@@ -61,12 +61,12 @@ public class Nodo {
 	public static void main(String[] args) {
 		// criar lista
 		Nodo head = new Nodo(), novo;
-		head.setData(3);
+		head.setData(2);
 		
-		novo = new Nodo(1);
+		novo = new Nodo(3);
 		insertAfter(novo, head);
 		
-		novo = new Nodo(2);
+		novo = new Nodo(1);
 		insertBefore(head, novo);
 		
 		head = novo;
