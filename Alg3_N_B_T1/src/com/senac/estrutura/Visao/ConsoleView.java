e86cf7e27ba4d7d5288798d86b4e31299b0db00c
Jeferson Tigik <jefersontigik@gmail.com>
2014-09-09 19:21:21 -0300

    Atualização repositório

--- Alg3_N_B_T1/src/com/senac/estrutura/Visao/ConsoleView.java
+++
@@ -1,9 +0,0 @@
-package com.senac.estrutura.Visao;
-
-public class ConsoleView {
-
-	public void imprimeInteiro(Integer valor) {
-		System.out.println(valor);
-	}
-
-}
--- Hello World/src/teste_java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package teste_java;
-
-public class HelloWorld {
-	 
-	  public void imprimeTexto(String texto){
-	      System.out.println(texto);
-	  }
-}
